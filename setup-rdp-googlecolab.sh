143e48c80378c6b99779ae44f201907f0326601d
RizFurr <rizzowohq@gmail.com>
2022-11-06 21:10:13 +0800

    Update setup-rdp-googlecolab.sh

--- setup-rdp-googlecolab.sh
+++ setup-rdp-googlecolab.sh
@@ -1,5 +1,5 @@
 echo "Download ngrok"
-wget https://raw.githubusercontent.com/akuhnet/w-colab/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+wget https://raw.githubusercontent.com/RizzyDev81/mystorage/main/ngrok-region.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo Downloading File From RizFurr mystorage github
 echo "===================================="
 echo "Wait 2 Minutes"
@@ -7,17 +7,17 @@ echo "This is only for education"
 echo "===================================="
 sudo apt-get update > /dev/null 2>&1
 echo "===================================="
-echo "Install Firefox"
+echo "Installing Firefox"
 echo "===================================="
 sudo apt install firefox -y > /dev/null 2>&1
 echo "===================================="
-echo "Install RDP"
+echo "Installing RDP"
 echo "===================================="
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
 sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
 echo "===================================="
-echo "Start RDP"
+echo "Starting RDP"
 echo "===================================="
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1

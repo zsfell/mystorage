9500d1173dd10f82d417b7e134b6087ceab07972
RizFurr <rizzowohq@gmail.com>
2022-11-07 00:30:11 +0800

    Create vps-setup.sh

---
+++ vps-bilek/vps-setup.sh
@@ -0,0 +1,24 @@
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo "Setup Ngrok US Region"    
+./ngrok tcp --region $CRP 22 &>/dev/null &
+echo "======================="
+echo "Updating Please Wait"
+echo "======================="
+sudo apt update > /dev/null 2>&1
+sudo apt install openssh-server > /dev/null 2>&1
+mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+sudo service ssh start
+echo "===================================="
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo Create Root Password
+passwd
+echo "===================================="

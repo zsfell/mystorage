a7e0a9524f4e34f990c24441b8ad871ac3c03423
RizFurr <rizzowohq@gmail.com>
2022-11-06 21:03:37 +0800

    Create ngrok-region.sh

---
+++ ngrok-region.sh
@@ -0,0 +1,15 @@
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "Best region us - United States (Ohio)"
+echo "auto set to us region"  
+./ngrok tcp --region us 3389 &>/dev/null &
+echo "======================="

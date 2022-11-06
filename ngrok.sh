c57fabbbb6334f36c2e1951363931abd90b296fe
RizFurr <rizzowohq@gmail.com>
2022-11-06 23:58:21 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -7,9 +7,10 @@ chmod +x ng.sh
 ./ng.sh
 clear
 echo "======================="
-echo choose ngrok region
+echo "Auto Choose Ngrok Region"
 echo "======================="
 echo "Best region us - United States (Ohio)"
 echo "auto set to us region"  
 ./ngrok tcp --region us 3389 &>/dev/null &
 echo "======================="
+clear

b91ae31e307335c9d4d7347c2ea8ec7af7495cda
RizFurr <rizzowohq@gmail.com>
2022-11-07 05:56:39 +0800

    Update vps-setup.sh

--- vps-bilek/vps-setup.sh
+++ vps-bilek/vps-setup.sh
@@ -19,6 +19,6 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 sudo service ssh start
 echo "===================================="
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo Create Root Password
-passwd
+echo create root password
+passwd rizfurr@123
 echo "===================================="

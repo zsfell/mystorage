d1ce4b84a5fe0ec9c35c435fc923b0151f3d430f
RizFurr <rizzowohq@gmail.com>
2022-11-17 23:57:07 +0800

    Update kali.sh

--- xrdp-file/kali.sh
+++ xrdp-file/kali.sh
@@ -1,21 +1,7 @@
 echo -e "$123\n$123\n" | sudo passwd
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3388 &>/dev/null &
+echo "Download Ngrok"
+wget https://raw.githubusercontent.com/RizzyDev81/mystorage/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+echo "===================================="
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
@@ -26,14 +12,11 @@ echo "Start RDP"
 echo "===================================="
 echo "===================================="
 echo "Username : ubuntu"
-echo "Password : ubuntu"
+echo "Password : rizz@123"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
 echo "===================================="
 echo "Don't close this tab to keep RDP running"
-echo "Keep support akuh.net thank you"
-echo "Wait 1 minute to finish bot"
-echo "===================================="
 echo "===================================="
 docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
